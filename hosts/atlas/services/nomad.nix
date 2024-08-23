1b912b27232c558fb46083b14c5bd01b596d24a9
amadd0x <amadd0x@github.com>
2024-08-23 16:27:15 -0500

    308 current 2024-08-23 16:15:08 23.11.20240709.205fd42 6.1.96 *

--- hosts/atlas/services/nomad.nix
+++ hosts/atlas/services/nomad.nix
@@ -38,6 +38,7 @@
       plugin = [
         {raw_exec = [{config = [{enabled = true;}];}];}
         {docker = [{config = [{volumes = [{enabled = true;}];}];}];}
+        {nomad-device-nvidia = [{config = [{enabled = true;}];}];}
       ];
       bind_addr = "0.0.0.0";
       data_dir = "/var/lib/nomad";
