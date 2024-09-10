510a0a87a7d9a253e27ee1eeca7619644bcbb0c4
amadd0x <amadd0x@github.com>
2024-09-10 12:14:45 -0500

    325 current 2024-08-26 09:42:13 23.11.20240709.205fd42 6.1.96 *

--- hosts/daedalus/services/networking.nix
+++ hosts/daedalus/services/networking.nix
@@ -8,12 +8,14 @@
     80
     81
     443
+    7777 # Satisfactory
     8080
     8081
     # 25565 # Minecraft
     # 25575 # Minecraft RCON
   ];
   networking.firewall.allowedUDPPorts = [
+    7777 # Satisfactory
     27000 # Avorion game port
     27003 # Avorion query port
     27021 # Avorion Steam port
