e9766764bd2105e060e7d9d011032faaff5272f9
amadd0x <amadd0x@github.com>
2025-07-18 18:32:05 -0500

    389 current 2025-07-18 09:07:24 24.11.20250630.50ab793 6.6.94 *

--- hosts/atlas/configuration.nix
+++ hosts/atlas/configuration.nix
@@ -110,6 +110,8 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  services.envfs.enable = true;
+
   # Enable sound with pipewire.
   # sound.enable = true;
   hardware.pulseaudio.enable = false;
