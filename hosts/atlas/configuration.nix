62f692bc0b0ae25a3a0263486d282254e781532a
amadd0x <amadd0x@github.com>
2026-05-11 09:09:08 -0500

    549 2026-05-11 09:06:47 25.11.20260505.0c88e1f 6.12.85 Unknown [] True

--- hosts/atlas/configuration.nix
+++ hosts/atlas/configuration.nix
@@ -72,6 +72,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   system.autoUpgrade = {
     enable = true;
